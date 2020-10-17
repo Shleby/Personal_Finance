222b980df8a1d3af8872aea808e0e62f803e7cb0
Shelby Huffman <shelbyhuffman@Shelbys-MacBook-Pro.local>
2020-10-16 22:04:13 -0500

    Server fully set up

--- client/src/components/Overview.tsx
+++ client/src/components/Overview.tsx
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Overview() {
-  return <div></div>;
+  const [hasError, setErrors] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [userData, setUserData] = useState([]);
+
+  async function fetchData() {
+    const res = await fetch("/users");
+    res
+      .json()
+      .then((res) => setUserData(res))
+      .catch((err) => setErrors(err));
+  }
+
+  useEffect(() => {
+    fetchData();
+    setLoading(false);
+  });
+
+  return (
+    <>
+      <h1>Users</h1>
+      {loading ? (
+        <div>Loading . . .</div>
+      ) : hasError ? (
+        <div>Error occured.</div>
+      ) : (
+        userData.map((data: any) => (
+          <div>
+            <h4>ID : {data.id}</h4>
+            <h4>Userame : {data.username}</h4>
+          </div>
+        ))
+      )}
+    </>
+  );
 }
